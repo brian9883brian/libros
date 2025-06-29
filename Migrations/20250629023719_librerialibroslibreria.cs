4c8df2770d4cb0945b45a21131c2d46120aadd92
brian9883brian <jbriantguerrero@gmail.com>
2025-06-28 20:53:38 -0600

    first commit

---
+++ Migrations/20250629023719_librerialibroslibreria.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Uttt.Micro.Libro.Migrations
+{
+    /// <inheritdoc />
+    public partial class librerialibroslibreria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Uttt.Micro.Libro.Extenciones;
+using Uttt.Micro.Libro.Persistencia;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar controladores
+builder.Services.AddControllers();
+
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3009")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// Swagger, DbContext y servicios personalizados
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ContextoLibreria>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCustomServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configurar Forwarded Headers para IP real
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+    KnownProxies = { IPAddress.Parse("172.21.0.1") }
+});
+
+// Swagger
+app.UseSwagger(c => { c.RouteTemplate = "swagger/{documentName}/swagger.json"; });
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+    c.RoutePrefix = "swagger";
+});
+app.MapOpenApi();
+
+// CORS
+app.UseCors("PermitirFrontend");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
